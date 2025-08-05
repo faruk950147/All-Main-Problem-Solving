00c9a6ea0cf77da3dcb9ec094fd9e4e297af6e5b
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-08-05 17:45:52 +0600

    Uploaded all files

--- Python practice/requests/3 post/main.py
+++ Python practice/requests/3 post/main.py
@@ -1,17 +1,22 @@
 import requests
 
 def post_data(url, data):
-    response = requests.post(url, data=data)
-    return response.json()
+    response = requests.post(url, json=data)  
+    if response.status_code == 201:  
+        return response.json()
+    else:
+        return {"error": f"Status code {response.status_code}"}
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/posts"
     data = {
         "title": "omar",
-        "body": "1234"
+        "body": "1234",
+        "userId": 1 
     }
     response = post_data(url, data)
     print(response)
+
 
 
 
@@ -23,4 +28,4 @@ if __name__ == "__main__":
 
 # response = requests.post(url, data=data)
 
-# print(response.text)
+# print(response.text)_data(url, data)
