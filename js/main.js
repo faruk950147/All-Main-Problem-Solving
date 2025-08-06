dd4aa41fd2d562790a70f8375b9ae347223b893f
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-08-06 17:10:03 +0600

    Uploaded all files

---
+++ js/main.js
@@ -0,0 +1,7 @@
+str = "Hello World";
+str1 = "Hello World";
+console.log(str.length);
+console.log(str1+" "+str);
+console.log(str.charAt(0));
+console.log(str1.charAt(0));
+console.log(str.slice(0, 5));
