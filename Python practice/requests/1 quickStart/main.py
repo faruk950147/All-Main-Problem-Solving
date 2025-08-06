9dd70f904509ca9e80bab4ea20d086d3d8dc2805
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-08-06 13:08:02 +0600

    Uploaded all files

--- Python practice/requests/1 quickStart/main.py
+++ Python practice/requests/1 quickStart/main.py
@@ -1,3 +1,21 @@
 import requests
-r = requests.get('https://api.github.com/events')
-print(r.text)
+from requests.exceptions import Timeout
+import os
+
+def WebGet(url):
+    try:
+        response = requests.get(url, timeout=1)
+        print(response.ok)
+        print(response.status_code)
+
+        # file write
+        file_path = r'L:\Programming\Python\All-Main Problem Solving\Python practice\requests\1 quickStart\python.html'
+        with open(file_path, "w+", encoding='utf-8') as file:
+            file.write(response.text)
+
+    except Timeout as e:
+        print("Timeout occurred:", str(e))
+    except requests.RequestException as e:
+        print("Request failed:", str(e))
+
+WebGet('http://python.org')
