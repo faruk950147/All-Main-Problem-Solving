bf2064174cbc87381f87ed0cea54d9488bf64606
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-08-08 17:50:26 +0600

    Uploaded all files

--- Python practice/Django/Projects/location tracking/map/views.py
+++ Python practice/Django/Projects/location tracking/map/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views import View
 from django.contrib import messages
 import phonenumbers
-from phonenumbers import timezone, geocoder as phone_geocoder, carrier
+from phonenumbers import timezone, geocoder, carrier
 from opencage.geocoder import OpenCageGeocode
 import folium
 from map.forms import NumberForm
@@ -17,29 +17,33 @@ class MapView(View):
         if form.is_valid():
             number = form.cleaned_data['number']
             api_key = "992f1a79901b4948892a93c128f9f515"    
+            try:
+                phone_number = phonenumbers.parse(number)
+                valid = phonenumbers.is_valid_number(phone_number)
+                if valid:
+                    location = geocoder.description_for_number(phone_number, "en")
+                    time_zone = timezone.time_zones_for_number(phone_number)
+                    carrier_name = carrier.name_for_number(phone_number, "en")
+                    result = OpenCageGeocode(api_key).geocode(location)
+                    lat = result[0]['geometry']['lat']
+                    lng = result[0]['geometry']['lng']
 
-            phone_number = phonenumbers.parse(number)
-            time_zone = timezone.time_zones_for_number(phone_number)
-            carrier_name = carrier.name_for_number(phone_number, 'en')
-            location = phone_geocoder.description_for_number(phone_number, 'en')
+                    # Folium Map as HTML
+                    map_object = folium.Map(location=[lat, lng], zoom_start=15)
+                    folium.Marker([lat, lng]).add_to(map_object)
+                    map_html = map_object._repr_html_()
 
-            result = OpenCageGeocode(api_key).geocode(location)
-            lat = result[0]['geometry']['lat']
-            lng = result[0]['geometry']['lng']
-
-            # Folium Map as HTML
-            map_object = folium.Map(location=[lat, lng], zoom_start=15)
-            folium.Marker([lat, lng]).add_to(map_object)
-            map_html = map_object._repr_html_()
-
-            messages.success(request, 'Location found successfully!')
-            return render(request, 'map/map.html', {
-                'form': form,
-                'map': map_html,
-                'time_zone': time_zone,
-                'carrier_name': carrier_name,
-                'location': location
-            })
+                    messages.success(request, 'Location found successfully!')
+                    return render(request, 'map/map.html', {
+                    'form': form,
+                    'map': map_html,
+                    'location': location,
+                    'time_zone': time_zone,
+                    'carrier_name': carrier_name,
+                    })
+            except Exception as e:
+                messages.error(request, str(e))
+                return render(request, 'map/map.html', {'form': form})
         return render(request, 'map/map.html', {'form': form})
 
             
