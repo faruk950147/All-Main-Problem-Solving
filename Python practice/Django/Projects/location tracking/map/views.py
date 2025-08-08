9e59cbdfd4b5204917a150417178931062cff12e
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-08-08 18:49:11 +0600

    Uploaded all files

--- Python practice/Django/Projects/location tracking/map/views.py
+++ Python practice/Django/Projects/location tracking/map/views.py
@@ -5,8 +5,9 @@ import phonenumbers
 from phonenumbers import timezone, geocoder, carrier
 from opencage.geocoder import OpenCageGeocode
 import folium
-from map.forms import NumberForm
 from config.settings import OPENCAGE_API_KEY
+from map.forms import NumberForm
+
 
 class MapView(View):
     def get(self, request):
@@ -18,7 +19,6 @@ class MapView(View):
 
         if form.is_valid():
             number = form.cleaned_data['number']
-
             try:
                 phone_number = phonenumbers.parse(number)
             except phonenumbers.NumberParseException:
@@ -32,27 +32,26 @@ class MapView(View):
             location = geocoder.description_for_number(phone_number, "en") or "Unknown"
             time_zone = ", ".join(timezone.time_zones_for_number(phone_number)) or "Unknown"
             carrier_name = carrier.name_for_number(phone_number, "en") or "Unknown"
-            
-            results = OpenCageGeocode(OPENCAGE_API_KEY).geocode(location)
-            
-            if results and len(results):
-                latitude = results[0]['geometry']['lat']
-                longitude = results[0]['geometry']['lng']
 
+            try:
+                results = OpenCageGeocode(OPENCAGE_API_KEY).geocode(location)
+            except Exception as e:
+                messages.error(request, f"Geocoding API error: {e}")
+                return render(request, 'map/map.html', context)
+
+            if results and len(results):
+                latitude = results[0]['geometry']['lat'] # latitude
+                longitude = results[0]['geometry']['lng'] # longitude
                 folium_map = folium.Map(location=[latitude, longitude], zoom_start=9)
                 folium.Marker([latitude, longitude], popup=location).add_to(folium_map)
-
-                map_html = folium_map._repr_html_()
-                context['map'] = map_html
+                context['map'] = folium_map._repr_html_()
+                messages.success(request, "Phone number information found!")
+                context.update({
+                    'location': location,
+                    'time_zone': time_zone,
+                    'carrier_name': carrier_name,
+                })
             else:
                 messages.warning(request, "Location info not found.")
-
-            messages.success(request, "Phone number information found!")
-
-            context.update({
-                'location': location,
-                'time_zone': time_zone,
-                'carrier_name': carrier_name,
-            })
 
         return render(request, 'map/map.html', context)
